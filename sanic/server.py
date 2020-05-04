2ecf9f0f48528d447b3f520386e82c1c578dd6b8
Adamant Mischke <mischkeaa@gmail.com>
2020-05-04 12:56:26 -0500

    some changes

--- sanic/server.py
+++ sanic/server.py
@@ -1,4 +1,6 @@
-from sanic import Sanic
+import os
+import glob
+from sanic import Sanic, response
 
 app = Sanic()
 
@@ -9,6 +11,20 @@ app.static("/static", "/usr/src/app/static")
 
 def main():
     app.run(host="0.0.0.0", port=8000, workers=4)
+
+
+@app.route("/farm")
+def farm(request):
+    pic_filenames = [
+        os.path.basename(x) for x in glob.glob("/usr/src/app/static/farm/*")
+    ]
+
+    html = ""
+    for pic in pic_filenames:
+        html = "{} <img src='/static/farm/{}' height=auto width=40%></img>".format(
+            html, pic
+        )
+    return response.html(html)
 
 
 if __name__ == "__main__":
